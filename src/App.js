c99ce8053c6924a7f0eba9cfb804e4ed3a1e83cb
David <martindavid99@gmail.com>
2026-01-07 01:11:35 -0600

    mapping animals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+    const [animals, setAnimals] = useState([]);
     const handleClick = () => {
-        console.log("Button was clicked");
+        setAnimals([...animals, getRandomAnimal()]);
+
     };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>
+                {renderedAnimals}
+            </div>
         </div>
     );
 }
